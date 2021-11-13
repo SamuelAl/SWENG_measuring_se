2742bac6d82bc24bcc954025b36e3d07f6069272
einstenz <33717014+einstenz@users.noreply.github.com>
2021-11-13 19:26:45 +0000

    refactored get commits functionality into basic function.

--- gather.py
+++ gather.py
@@ -8,11 +8,10 @@ g = Github("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")
 def get_user_repos(usr_name):
     return g.get_user("usr_name")
 
-# Get info for a specific repo (SamuelAl/GraphApp-Stocks-Visualizer)
-repo_name = "SamuelAl/libfive"
-libfive = g.get_repo(repo_name)
-for commit in libfive.get_commits():
-    pprint.pprint(commit)
+def get_repo_commits(repo_name):
+    return g.get_repo(repo_name).get_commits()
+
+
 
 
 
