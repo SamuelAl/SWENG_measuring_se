a3d2bf471b24a5c80a39c4dac0996121733f97af
einstenz <33717014+einstenz@users.noreply.github.com>
2021-11-18 14:41:28 +0000

    added parameters to api call

--- data_api/src/api.py
+++ data_api/src/api.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 def welcome():
     return "Hello!"
 
-@app.route('/data/', methods=['GET','POST'])
-def get_stats():
-    return jsonpickle.encode(get_commit_stats_by_date("SamuelAl/hexbin"))
+@app.route('/data/user/<string:username>/repo/<string:repo_name>/', methods=['GET','POST'])
+def get_stats(username,repo_name):
+    full_repo_name = username + "/" + repo_name
+    return jsonpickle.encode(get_commit_stats_by_date(full_repo_name))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
