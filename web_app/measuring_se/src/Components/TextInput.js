cb53457c1f790389a42bb91f0f4526ff298d2825
SamuelAl <s.alarco@outlook.com>
2021-11-19 10:47:36 +0000

    Create text input component

---
+++ web_app/measuring_se/src/Components/TextInput.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function TextInput(params) {
+    const label, type, id, name, placeholder, onChange = params
+    return (
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            {label}
+          </label>
+          <div className="mt-1">
+            <input
+              type={type}
+              name={name}
+              id={id}
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+              placeholder={placeholder}
+              onChange={placeholder}
+            />
+          </div>
+        </div>
+      )
+}
