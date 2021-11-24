e0442d23bcef707fce6645b9c800ad35cf7bf972
SamuelAl <s.alarco@outlook.com>
2021-11-24 20:28:57 +0000

    Reset selected contributor when searching new repo

--- web_app/measuring_se/src/Components/Dashboard.js
+++ web_app/measuring_se/src/Components/Dashboard.js
@@ -78,6 +78,14 @@ export default class Dashboard extends Component {
         }, () => this.fetchCommitStats())
     }
 
+    handleSearch = () => {
+        this.setState({
+            contributor: null
+        }, () => {
+            this.fetchData()
+        })
+    }
+
     processData = (rawData) => {
         return Object.keys(rawData)
             .map((key) => {
@@ -180,7 +188,7 @@ export default class Dashboard extends Component {
                                     <button
                                         type="button"
                                         className="inline-flex h-10 items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                        onClick={this.fetchData}
+                                        onClick={this.handleSearch}
                                     >
                                         <SearchIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
                                         Search
