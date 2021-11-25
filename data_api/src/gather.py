233c234ec2e0e24195b642f01136a5e9fd4bc723
SamuelAl <s.alarco@outlook.com>
2021-11-25 21:48:53 +0000

    Implemented get commit stats using mongodb

--- data_api/src/gather.py
+++ data_api/src/gather.py
@@ -1,129 +1,124 @@
 from datetime import datetime
 from github import Github
 import json
+import jsonpickle
 import pprint
 import pymongo
+
+ALL_AUTHORS = "all"
 
 g = Github("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")
 
 # Establish connection to database
-conn = "mongo:27017"
-client = pymongo.MongoClient('localhost',27017)
+
+client = pymongo.MongoClient('localhost', 27017)
 
 db = client.github_database
 repos_collection = db.repos
+repos_collection.create_index([("repo_name", pymongo.ASCENDING)], unique=True)
+commit_stats_collection = db.commit_stats
 
-
-class CommitStats:
-    def __init__(self, additions, changes, deletions):
-        self.additions = additions
-        self.changes = changes
-        self.deletions = deletions
-        self.standardized = False
-
-    def __str__(self):
-        return f'Additions: {self.additions}; Changes: {self.changes}; Deletions: {self.deletions}'
-    
-    def standardize(self): 
-        self.additions = self.additions / self.changes
-        self.deletions = self.deletions / self.changes
-        self.standardized = True
-
-class ContributorInfo:
-    def __init__(self, login, avatar_url):
-        self.login = login
-        self.avatar_url = avatar_url
-    
-    def __str__(self):
-        return f'Login: {self.login}; Avatar URL: {self.avatar_url}'
-
+def stats_add(a, b):
+    a["additions"] += b["additions"]
+    a["changes"] += b["changes"]
+    a["deletions"] += b["deletions"]
 
 def calculate_commit_stats(files):
-    stats = CommitStats(0,0,0)
+    stats = {
+        "additions": 0,
+        "deletions": 0,
+        "changes": 0,
+    }
     for file in files:
-        stats.additions += file.additions
-        stats.changes += file.changes
-        stats.deletions += file.deletions
+        stats["additions"] += file.additions
+        stats["changes"] += file.changes
+        stats["deletions"] += file.deletions
     return stats
+
 
 def get_user_repos(usr_name):
     return g.get_user("usr_name")
 
+
 def get_repo_commits(repo_name):
     return g.get_repo(repo_name).get_commits()
 
+
 def get_repo_contributors(repo_name):
-    contributor_dct = {} 
+    contributors_arr = []
     contributors = g.get_repo(repo_name).get_contributors()
     for contributor in contributors:
-        contributor_dct[contributor.login] = ContributorInfo(contributor.login, contributor.avatar_url)
-    return contributor_dct
+        c_dct = {
+            "name": contributor.login,
+            "avatar_url": contributor.avatar_url
+        }
+        contributors_arr.append(c_dct)
+    return contributors_arr
 
 
-def get_commit_stats_by_date(repo_name, normalized):
-    date_dct = {} 
+def get_commit_stats_from_api(repo_name):
+    commits_dct = {}
     commits = get_repo_commits(repo_name)
-    print(commits.totalCount)
+    # Get commit stats
     for commit in commits:
+        author = commit.author.login
         files = commit.files
         stats = calculate_commit_stats(files)
         date = commit.commit.author.date
         date_str = date.strftime("%Y/%m/%d")
-        if date_str in date_dct:
-            date_stats = date_dct[date_str]
-            date_stats.additions += stats.additions
-            date_stats.changes += stats.changes
-            date_stats.deletions += stats.deletions
-            date_dct[date_str] = date_stats
+        if date_str in commits_dct:
+            date_dct = commits_dct[date_str]
         else:
-            date_dct[date_str] = CommitStats(stats.additions,stats.changes,stats.deletions)
-    
-    if normalized:
-        for k,v in date_dct.items():
-            v.standardize()
-            date_dct[k] = v
-    
-    return date_dct
+            date_dct = {}
+            date_dct[ALL_AUTHORS] = {
+                "additions": 0, "changes": 0, "deletions": 0}
 
-def get_commit_stats_by_date_user(repo_name, user, normalized):
-    date_dct = {} 
-    commits = get_repo_commits(repo_name)
-    print(commits.totalCount)
-    for commit in commits:
-        if commit.author.login != user:
-            continue
-        files = commit.files
-        stats = calculate_commit_stats(files)
-        date = commit.commit.author.date
-        date_str = date.strftime("%Y/%m/%d")
-        if date_str in date_dct:
-            date_stats = date_dct[date_str]
-            date_stats.additions += stats.additions
-            date_stats.changes += stats.changes
-            date_stats.deletions += stats.deletions
-            date_dct[date_str] = date_stats
+        if author in date_dct:
+            author_stats = date_dct[author]
         else:
-            date_dct[date_str] = CommitStats(stats.additions,stats.changes,stats.deletions)
-    
-    if normalized:
-        for k,v in date_dct.items():
-            v.standardize()
-            date_dct[k] = v
-    
-    return date_dct
+            author_stats = {"additions": 0, "changes": 0, "deletions": 0}
 
-def get_commit_stats_mongo(repo_name, user, normalized):
-    # Initializing dictionary with repo name
-    repo_dct = {
-        repo_name: repo_name
-    }
-    # Get repo contributors
-    contributors = get_repo_contributors(repo_name)
+        general_stats = date_dct[ALL_AUTHORS]
+
+        stats_add(general_stats, stats)
+        stats_add(author_stats, stats)
+
+        date_dct[ALL_AUTHORS] = general_stats
+        date_dct[author] = author_stats
+        commits_dct[date_str] = date_dct
+
+    commit_stats_arr = []
+    for date, stats in commits_dct.items():
+        for user, stat in stats.items():
+            commit_stat_doc = {
+                "_id": repo_name + user + date,
+                "repo_name": repo_name,
+                "date": date,
+                "granularity": "by_day",
+                "contributor": user,
+                "stats": stat
+            }
+            commit_stats_arr.append(commit_stat_doc)
+
+    return commit_stats_arr
 
 
+def get_commit_stats(repo_name, user):
+    is_in_db = repos_collection.count_documents({"repo_name": repo_name}) >= 1
+    stats = []
+    if is_in_db:
+        stats = commit_stats_collection.find(
+            {"repo_name": repo_name, "contributor": user})
+    else:
+        repo_doc = {
+            "repo_name": repo_name,
+            "contributors": get_repo_contributors(repo_name),
+        }
+        repos_collection.insert_one(repo_doc)
+        unfltr_stats = get_commit_stats_from_api(repo_name)
+        commit_stats_collection.insert_many(unfltr_stats)
+        stats = [a for a in unfltr_stats if a["contributor"] == user]
+    return stats
 
 
-
-
-
-
+get_commit_stats("SamuelAl/hexbin", "SamuelAl")
