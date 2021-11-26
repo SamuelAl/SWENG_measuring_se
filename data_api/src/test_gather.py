27cd4510246bd6d5645e3582dfc19482d9ff061c
SamuelAl <s.alarco@outlook.com>
2021-11-26 09:44:48 +0000

    Added tests for commits from api

--- data_api/src/test_gather.py
+++ data_api/src/test_gather.py
@@ -1,5 +1,7 @@
 from gather import *
 
+TEST_REPO_NAME = "SamuelAl/test_repo"
+
 def test_stats_add():
     # Normal case
     a = {"additions": 1,"changes": 2,"deletions": 1,}
@@ -46,7 +48,7 @@ def test_normalize_commit_stat():
 
 def test_get_repo_contributors():
     # Get a personal repo with a single contributor
-    arr = get_repo_contributors("SamuelAl/test_repo")
+    arr = get_repo_contributors(TEST_REPO_NAME)
     assert len(arr) == 1
     assert arr[0]["name"] == "SamuelAl"
     assert arr[0]["avatar_url"] == "https://avatars.githubusercontent.com/u/33717014?v=4"
@@ -54,6 +56,46 @@ def test_get_repo_contributors():
     # Get repo with multiple contributors
     arr = get_repo_contributors("SamuelAl/Clothes-Annotation-Web-App")
     assert len(arr) == 7
+
+def test_get_commits_from_api():
+    # Use test repo 
+    arr = get_commit_stats_from_api(TEST_REPO_NAME)
+
+    assert len(arr) == 2
+
+    c_all = c_contributor = []
+    if arr[0]["contributor"] == "all":
+        c_all = arr[0]
+        c_contributor = arr[1]
+    else:
+        c_all = arr[1]
+        c_contributor = arr[0]
+    
+    # Check contributor field
+    assert c_all["contributor"] == "all"
+    assert c_contributor["contributor"] == "SamuelAl"
+
+    # Check date field
+    assert c_all["date"] == "2021/11/26"
+    assert c_contributor["date"] == "2021/11/26"
+
+    # Check granularity
+    assert c_all["granularity"] == "by_day"
+    assert c_contributor["granularity"] == "by_day"
+
+    # Check stats
+    stats_all = c_all["stats"]
+    stats_contributor = c_contributor["stats"]
+
+    assert stats_all["additions"] == 1
+    assert stats_all["changes"] == 1
+    assert stats_all["deletions"] == 0
+
+    assert stats_contributor["additions"] == 1
+    assert stats_contributor["changes"] == 1
+    assert stats_contributor["deletions"] == 0
+
+
 
 
 
